62d47f0eb8eeb16891e21ddc8dbfe2fdb50d3538
Alejandro Klever <45394625+alejandroklever@users.noreply.github.com>
2022-12-15 16:56:53 -0500

    Update ShoppingCartTests.java

--- src/test/java/test/strategio/ShoppingCartTests.java
+++ src/test/java/test/strategio/ShoppingCartTests.java
@@ -1,6 +1,6 @@
 package test.strategio;
 
-import wwwwwworg.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
